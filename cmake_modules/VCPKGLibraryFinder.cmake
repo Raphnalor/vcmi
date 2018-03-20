314292ad59338f49a3e8415b69f11ce81f681e5d
Raphnalor <raphnalor@gmail.com>
2018-03-20 22:25:01 +0300

    Replace CMake find_library() function for VCPKG #3.

--- cmake_modules/VCPKGLibraryFinder.cmake
+++ cmake_modules/VCPKGLibraryFinder.cmake
@@ -4,6 +4,7 @@ if (NOT _VCPKG_ROOT_DIR)
   return()
 endif()
 
+# Parses <VAR> and names arguments of the find_library() function.
 function(_vcpkg_parse_find_library_arguments outputVar outputNames #[[arguments]])
   set(var)
   set(names)
@@ -15,20 +16,21 @@ function(_vcpkg_parse_find_library_arguments outputVar outputNames #[[arguments]
       list(GET arguments 0 arg)
       list (REMOVE_AT arguments 0)
       if (arg STREQUAL "NAMES")
-        set (stopList NAMES_PER_DIR NO_DEFAULT_PATH NO_CMAKE_PATH NO_CMAKE_ENVIRONMENT_PATH
-                  NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH CMAKE_FIND_ROOT_PATH_BOTH
-                  ONLY_CMAKE_FIND_ROOT_PATH NO_CMAKE_FIND_ROOT_PATH DOC HINTS PATHS PATH_SUFFIXES)
+        set (stopList "NAMES_PER_DIR" "NO_DEFAULT_PATH" "NO_CMAKE_PATH" "NO_CMAKE_ENVIRONMENT_PATH"
+                      "NO_SYSTEM_ENVIRONMENT_PATH" "NO_CMAKE_SYSTEM_PATH" "CMAKE_FIND_ROOT_PATH_BOTH"
+                      "ONLY_CMAKE_FIND_ROOT_PATH" "NO_CMAKE_FIND_ROOT_PATH" "DOC" "HINTS" "PATHS"
+                      "PATH_SUFFIXES")
         while (arguments)
           list(GET arguments 0 arg)
           list (REMOVE_AT arguments 0)
-          list (FIND stopList arg idx)
+          list (FIND stopList "${arg}" idx)
           if (NOT idx EQUAL -1)
             break()
           endif()
-          list(APPEND names ${arg})
+          list(APPEND names "${arg}")
         endwhile()
       else()
-        set(names ${arg})
+        set(names "${arg}")
       endif()
     endif()
   endif()
@@ -36,41 +38,50 @@ function(_vcpkg_parse_find_library_arguments outputVar outputNames #[[arguments]
   set (${outputNames} ${names} PARENT_SCOPE)
 endfunction()
 
+# Appends a list of library names with possible debug names.
 function (_vcpkg_append_library_debug_names inOutNames)
   set (names ${${inOutNames}})
-  set (resultNames)
+  set (namesWithD)
+  set (namesWithDashDebug)
   foreach (name ${names})
-    list(APPEND resultNames "${name}" "${name}d" "${name}-debug")
+    list(APPEND namesWithD "${name}d")
+    list(APPEND namesWithDashDebug "${name}-debug")
   endforeach()
-  set (${inOutNames} ${resultNames} PARENT_SCOPE)
+  set (${inOutNames} ${names} ${namesWithD} ${namesWithDashDebug} PARENT_SCOPE)
 endfunction()
 
 function(find_library #[[arguments]])
   _vcpkg_parse_find_library_arguments(var names ${ARGN})
   if (NOT var OR NOT names)
+    # Could not parse arguments, let native find_library() print error.
     _find_library(${ARGN})
     return()
   endif()
 
   if (${var})
+    # The library is found and its path is cached already.
     return()
   endif()
 
+  # Find release version of the library.
   _find_library(_LIBPATH_RELEASE
     NAMES
       ${names}
     HINTS
-      ${_VCPKG_ROOT_DIR}/installed/${VCPKG_TARGET_TRIPLET}/lib
+      "${_VCPKG_ROOT_DIR}/installed/${VCPKG_TARGET_TRIPLET}/lib"
+      "${_VCPKG_ROOT_DIR}/installed/${VCPKG_TARGET_TRIPLET}/lib/manual-link"
     NO_DEFAULT_PATH
   )
 
+  # Find debug version of library.
   set(debugNames ${names})
   _vcpkg_append_library_debug_names(debugNames)
   _find_library(_LIBPATH_DEBUG
     NAMES
       ${debugNames}
     HINTS
-      ${_VCPKG_ROOT_DIR}/installed/${VCPKG_TARGET_TRIPLET}/debug/lib
+      "${_VCPKG_ROOT_DIR}/installed/${VCPKG_TARGET_TRIPLET}/debug/lib"
+      "${_VCPKG_ROOT_DIR}/installed/${VCPKG_TARGET_TRIPLET}/debug/lib/manual-link"
     NO_DEFAULT_PATH
   )
 
@@ -85,10 +96,12 @@ function(find_library #[[arguments]])
   unset(_LIBPATH_DEBUG CACHE)
 
   if (NOT libPaths)
+    # Could not find library, let native find_library() try.
     _find_library(${ARGN})
     return()
   endif()
 
+  # Was able to find library.
   set (${var} "${libPaths}" CACHE STRING "Paths to optimized and debug libraries.")
 endfunction()
 
